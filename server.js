326933c1e39b23b08600f7cb77899ff3d4474988
Solace <monkdev9@gmail.com>
2026-01-21 23:49:40 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // server.js
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => console.error('MongoDB connection error:', err));
 const express = require('express');
 const next = require('next');
 const compression = require('compression');
