72dceefb2347177bbff86a6d6e16ba4e7a2e561d
Solace <monkdev9@gmail.com>
2026-03-07 20:11:23 +0000

    Update route.ts

--- apps/web/app/api/contracts/[id]/route.ts
+++ apps/web/app/api/contracts/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '../../auth/[...nextauth]/route';
 import { connectDB } from '../../../../lib/mongoose';
 import { ContractModel } from '../../../../models/contract';
 import { ContractVersionModel } from '../../../../models/contractVersion';
